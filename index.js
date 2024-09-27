ca7c285006e3148dfe9dea33f49611688c1fc04d
Roshan Singh bhadauriya <roshanbhadoriya178@gmail.com>
2024-09-27 21:55:11 +0530

    complitted addd remove bounty functionality

---
+++ index.js
@@ -0,0 +1,66 @@
+const form =document.querySelector('form');
+let id=0;
+
+const bountyArea = document.querySelector(".bounty-area");
+
+
+
+function addtodo(value){
+       let newDiv =document.createElement('div');
+       let p =document.createElement('p');
+       p.className= "para";
+       p.appendChild(document.createTextNode(value));
+       newDiv.id= id;
+       newDiv.className= "bounty";
+         let edit = document.createElement("a");
+         edit.className = "edit";
+         edit.appendChild(document.createTextNode("edit"));
+         let deleteit = document.createElement("a");
+         deleteit.className = "delete";
+         deleteit.appendChild(document.createTextNode("delete"));
+         newDiv.appendChild(p);
+       newDiv.appendChild(edit);
+       newDiv.appendChild(deleteit);
+       bountyArea.appendChild(newDiv);
+       bountyArea.style.padding = "1rem";
+}
+
+
+form.addEventListener('submit',function(e){
+        e.preventDefault();
+        id++;
+        const value =document.querySelector('#todos').value ;
+      addtodo(value);
+      document.querySelector('#todos').value ="";
+      
+})
+
+/// delete todo
+document.querySelector(".bounty-area").addEventListener("click" , function(e){
+ 
+ if(e.target.className === "delete"){
+    let parent = e.target.parentNode;
+    parent.remove();
+    alert("your bounty is deleted");
+ }
+  
+})
+
+
+/// edit tododocument.querySelector(".bounty-area").addEventListener("click" , function(e){
+
+document.querySelector(".bounty-area").addEventListener("click" , function(e){
+ 
+  if(e.target.className === "edit"){
+     let parent = e.target.parentNode;
+     let parentId = parent.id;
+     console.log(parentId)
+     let first = parent.firstChild;
+     console.log(first);
+     document.querySelector('#todos').value = `${first.innerHTML}`;
+     parent.remove(); 
+  }
+ 
+ })
+   
+ 
